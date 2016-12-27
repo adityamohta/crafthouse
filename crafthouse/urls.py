735c0a9bc43aed225b372ee35002ff15bb2b6ee3
root <root@crafthouse.localdomain>
2016-12-27 21:51:45 +0000

    updated

--- crafthouse/urls.py
+++ crafthouse/urls.py
@@ -21,8 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('portfolio.urls', namespace='portfolio')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# if settings.DEBUG:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
